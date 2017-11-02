75cbb74caa8662f7c0bc49f54d2c3a5cd0148259
Yahya Hussein <yahyahussein91@gmail.com>
2017-11-01 19:32:14 -0700

    set up method for getting input from users

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -7,32 +7,19 @@ public class Main {
     //Scanner for user input
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        int[] myIntegers = getIntegers(5);
-        for(int i = 0; i < myIntegers.length; i++){
-            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
-        }
-        System.out.println("The average is " + getAverage(myIntegers));
-
+        //Define array of 10 integers
+        int[] myIntegers = getIntegers(10);
     }
 
+    //get integers from user method
     public static int[] getIntegers(int number){
-        System.out.println("Enter " + number + " integer values. \r");
-        //define array
+        System.out.println("Enter integers " + number + " integer values. \r" );
+        //define array from user input
         int[] values = new int[number];
         for(int i = 0; i < values.length; i++){
             values[i] = scanner.nextInt();
         }
         return values;
-    }
-
-    //method for calculating average of numbers
-    public static double getAverage(int[] array){
-        int sum = 0;
-        for(int i = 0; i < array.length; i++){
-            sum += array[i];
-        }
-        //cast to double
-        return (double) sum / (double)array.length;
     }
 }
 
@@ -61,3 +48,31 @@ public class Main {
 //        for(int i = 0; i < array.length; i++){
 //            System.out.println("Element " + i + ", value is " + array[i]);
 //        }
+
+
+//   int[] myIntegers = getIntegers(5);
+//        for(int i = 0; i < myIntegers.length; i++){
+//            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
+//        }
+//        System.out.println("The average is " + getAverage(myIntegers));
+//
+//    }
+//
+//    public static int[] getIntegers(int number){
+//        System.out.println("Enter " + number + " integer values. \r");
+//        //define array
+//        int[] values = new int[number];
+//        for(int i = 0; i < values.length; i++){
+//            values[i] = scanner.nextInt();
+//        }
+//        return values;
+//    }
+//
+//    //method for calculating average of numbers
+//    public static double getAverage(int[] array){
+//        int sum = 0;
+//        for(int i = 0; i < array.length; i++){
+//            sum += array[i];
+//        }
+//        //cast to double
+//        return (double) sum / (double)array.length;
