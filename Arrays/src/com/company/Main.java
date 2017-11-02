d9c7678a7781ba100c0184d6e886ac9e4b6ac213
Yahya Hussein <yahyahussein91@gmail.com>
2017-11-01 19:14:56 -0700

    added methods and values for arrays

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -1,31 +1,52 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    //Scanner for user input
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-	// write your code here
-        //Define Array
-        int[] myIntArray = new int[25]; //{1,2,3,4,5,6,7,8,9,10};
-        //Save value of array
-        double[] myDoubleArray = new double[10];
-        //Retrieve value from array
-//        System.out.println(myIntArray[5]);
-//        System.out.println(myIntArray[6]);
-//        System.out.println(myIntArray[8]);
-        //For loop to retrieve values
-        for(int i = 0; i < myIntArray.length; i++){
-            myIntArray[i] = i * 10;
+        int[] myIntegers = getIntegers(5);
+        for(int i = 0; i < myIntegers.length; i++){
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
         }
-        printArray(myIntArray);
+
     }
 
-    public static void printArray(int[] array){
-        for(int i = 0; i < array.length; i++){
-            System.out.println("Element " + i + ", value is " + array[i]);
+    public static int[] getIntegers(int number){
+        System.out.println("Enter " + number + " integer values. \r");
+        //define array
+        int[] values = new int[number];
+        for(int i = 0; i < values.length; i++){
+            values[i] = scanner.nextInt();
         }
+        return values;
     }
 }
 
 //Arrays allow you to store a sequence of values
 //Index starts at position 0
 //.length will give you length of array instead of hardcoding.
+
+
+// write your code here
+//        //Define Array
+//        int[] myIntArray = new int[25]; //{1,2,3,4,5,6,7,8,9,10};
+//        //Save value of array
+//        double[] myDoubleArray = new double[10];
+//        //Retrieve value from array
+////        System.out.println(myIntArray[5]);
+////        System.out.println(myIntArray[6]);
+////        System.out.println(myIntArray[8]);
+//        //For loop to retrieve values
+//        for(int i = 0; i < myIntArray.length; i++){
+//            myIntArray[i] = i * 10;
+//        }
+//        printArray(myIntArray);
+//    }
+//
+//    public static void printArray(int[] array){
+//        for(int i = 0; i < array.length; i++){
+//            System.out.println("Element " + i + ", value is " + array[i]);
+//        }
