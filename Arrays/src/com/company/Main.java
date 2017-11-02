5ad8e69c05c34b00e67b6c910f4eebe7c3ce1be2
Yahya Hussein <yahyahussein91@gmail.com>
2017-11-01 19:02:14 -0700

    covering basics of arrays in java

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -4,5 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //Define Array
+        int[] myIntArray = {1,2,3,4,5,6,7,8,9,10};
+        //Save value of array
+        double[] myDoubleArray = new double[10];
+        //Retrieve value from array
+//        System.out.println(myIntArray[5]);
+//        System.out.println(myIntArray[6]);
+//        System.out.println(myIntArray[8]);
+        //For loop to retrieve values
+        for(int i = 0; i < 10; i++){
+            myIntArray[i] = i * 10;
+        }
+        for(int i = 0; i < 10; i++){
+            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+        }
     }
 }
+
+//Arrays allow you to store a sequence of values
+//Index starts at position 0
