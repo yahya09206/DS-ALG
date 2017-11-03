5bab34377e2d86eff4ef8f1560936329994317fe
Yahya Hussein <yahyahussein91@gmail.com>
2017-11-02 20:22:25 -0700

    set up print instructions method

--- ArrayList/src/com/company/Main.java
+++ ArrayList/src/com/company/Main.java
@@ -40,4 +40,15 @@ public class Main {
         }
 
     }
+
+    public static void printInstructions() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of grocery items.");
+        System.out.println("\t 2 - To add an item to the list.");
+        System.out.println("\t 3 - To modify an item in the list.");
+        System.out.println("\t 4 - To remove an item from the list.");
+        System.out.println("\t 5 - To search for an item in the list.");
+        System.out.println("\t 6 - To quit the application.");
+    }
 }
