c93a6a948aa2b86fa5aa051812b817486fa83479
Yahya Hussein <yahyahussein91@gmail.com>
2017-11-02 20:02:16 -0700

    set up print list method

--- ArrayList/src/GroceryList.java
+++ ArrayList/src/GroceryList.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class GroceryList {
+
+    //declare new array list
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public void addGroceryItem(String item){
+        //Add element to array list
+        groceryList.add(item);
+    }
+
+    //print list method
+    public void printGroceryList(){
+        System.out.println("You have " + groceryList.size() + " in your grocery list");
+        for(int i = 0; i < groceryList.size(); i++){
+            System.out.println((i + 1) + ", " + groceryList.get(i));
+        }
+    }
 }
