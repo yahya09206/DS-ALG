a0e89a88603dcd63af1910e828d0962de2c0c595
Yahya Hussein <yahyahussein91@gmail.com>
2017-11-03 18:40:21 -0700

    set up add method

--- ArrayList/src/com/company/MobilePhone.java
+++ ArrayList/src/com/company/MobilePhone.java
@@ -10,4 +10,14 @@ public class MobilePhone {
         this.myNumber = myNumber;
         this.myContacts = new ArrayList<Contact>();
     }
+
+    public boolean addNewContact(Contact contact){
+        if(findContact(contact.getName()) >= 0){
+            System.out.println("Contact is already on file");
+            return false;
+        }
+
+        myContacts.add(contact);
+        return true;
+    }
 }
