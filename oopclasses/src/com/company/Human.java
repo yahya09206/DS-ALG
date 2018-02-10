be0113ab035805922fc572a4d0662c867260c358
Yahya Hussein <yahyahussein91@gmail.com>
2018-02-09 20:51:37 -0800

    set up methods for human class

--- oopclasses/src/com/company/Human.java
+++ oopclasses/src/com/company/Human.java
@@ -6,4 +6,24 @@ public class Human {
     int age;
     int heightInInches;
     String eyecolor;
+
+    //methods for different behaviors
+    public void speak(){
+        System.out.println("Hello, my name is " + name);
+        System.out.println("I am " + heightInInches + " inches tall");
+        System.out.println("I am " + age + " years old");
+        System.out.println("My eye color is " + eyecolor);
+    }
+
+    public void eat(){
+        System.out.println("Eating....");
+    }
+
+    public void walk(){
+        System.out.println("Walking...");
+    }
+
+    public void work(){
+        System.out.println("Working...");
+    }
 }
