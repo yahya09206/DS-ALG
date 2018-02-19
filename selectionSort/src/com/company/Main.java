0be82cd6504ca5e4f92e23023e8438f43e15f814
Yahya Hussein <yahyahussein91@gmail.com>
2018-02-18 20:36:15 -0800

    set up skeleton for selection sort

--- selectionSort/src/com/company/Main.java
+++ selectionSort/src/com/company/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int [] myArray = selectionSort(new int[] {9,8,3,13,87,,12,99});
+        for (int i=0; i <myArray.length; i++){
+            System.out.println(myArray[i]);
+        }
+
+    }
+
+    public static int [] selectionSort(int a[]){
+
     }
 }
