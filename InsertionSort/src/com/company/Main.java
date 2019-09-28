7dc70facb457c4a0840cbd877a5889b95863275a
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-27 18:35:18 -0700

    sorting method works

--- InsertionSort/src/com/company/Main.java
+++ InsertionSort/src/com/company/Main.java
@@ -4,5 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+
+            // Save value of element that's going to be inserted
+            int newElement = intArray[firstUnsortedIndex];
+
+            int i;
+
+            // Keep looking for insertion position as long as element in sorted partion > than element needed to be inserted. Decrement from left to left
+            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+                // Shift element to right to make room for new element
+                intArray[i] = intArray[i - 1];
+            }
+            // Assign with new element
+            intArray[i] = newElement;
+        }
+
+        for (int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
     }
 }
