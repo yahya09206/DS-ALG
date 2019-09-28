4335357bfcab19bb4b621a885bdd3e59e44beb55
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-27 18:08:16 -0700

    using code from bubble sort

---
+++ SelectionSort/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for (int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+    }
+
+    // Swap method
+    public static void swap(int[] array, int i, int j) {
+        //Check if i=j
+        if (i == j) {
+            return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
