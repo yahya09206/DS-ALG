090f970f956ed407e211fb0a25f2a335b112ab3a
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-28 22:51:25 -0700

    forgot to call sort method

--- QuickSort/src/com/company/Main.java
+++ QuickSort/src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22};
 
+        quickSort(intArray, 0, intArray.length);
 
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
