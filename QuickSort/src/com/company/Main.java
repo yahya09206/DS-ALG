9880739887f08b90dbaaef8e9746dd563df64b36
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-28 22:38:32 -0700

    starting on quicksort

---
+++ QuickSort/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22};
+
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+}
