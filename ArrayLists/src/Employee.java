b9e8231e79d21fb4bc4e65081469935a38fcabd4
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-29 16:59:41 -0700

    to string method

--- ArrayLists/src/Employee.java
+++ ArrayLists/src/Employee.java
@@ -9,4 +9,37 @@ public class Employee {
         this.lastName = lastName;
         this.id = id;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
