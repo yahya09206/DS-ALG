a233659d55cf7b83900aa1ae1a8a5a2fc66aa410
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-29 16:58:16 -0700

    new employee class

---
+++ ArrayLists/src/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private int id;
+
+    public Employee(String firstName, String lastName, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+}
