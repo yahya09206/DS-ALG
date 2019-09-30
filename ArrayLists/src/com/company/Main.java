02c36cbf3485ee6abdb60864a30a217541865f78
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-29 17:04:05 -0700

    created new instances of employee

--- ArrayLists/src/com/company/Main.java
+++ ArrayLists/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,5 +9,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("Jane", "Jones", 123));
+        employeeList.add(new Employee("John", "Doe", 4567));
+        employeeList.add(new Employee("Mary", "Smith", 22));
+        employeeList.add(new Employee("Mike", "Wilson", 3245));
+
     }
 }
