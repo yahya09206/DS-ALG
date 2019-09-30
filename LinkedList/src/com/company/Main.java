fe49eee2dc387028ae1038c78ebb11465f2d94e1
Yahya Hussein <yahyahussein91@gmail.com>
2019-09-29 17:52:48 -0700

    starting on linkedlists

---
+++ LinkedList/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Employee janeJones = new Employee("Jane", "Jones", 123));
+        Employee johnDoe = new Employee("John", "Doe", 4567));
+        Employee marySmith = new Employee("Mary", "Smith", 22));
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245));
+
+    }
+}
